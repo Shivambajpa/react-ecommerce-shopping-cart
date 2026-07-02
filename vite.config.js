b9f9b3351356a180be1b3c4da79733a2c31d3f29
Shivam bajpai <shivambajpai22122002@gmail.com>
2026-07-02 15:38:15 +0530

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: './'  // Vercel ke liye ye important hai
+  plugins: [react()],
+  base: '/ecommerce-project-2026/', 
 })
